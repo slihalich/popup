855aee3fd1ea4279ff247978fa2f37a7d17c65a2
slihalich <slihalich@users.noreply.github.com>
2019-03-12 13:02:36 +0300

    v3

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ overlay = d.getElementById('overlay'),
 open = d.getElementById('open'),
 popup = d.getElementById('popup');
 
-close.addEventListener('click', function() {
-  popup.classList.add('popupfade');
-  overlay.classList.add('overlayfade');
-  setTimeout(function() {
-    overlay.classList.add('none');
-    popup.classList.remove('popupfade');
-    overlay.classList.remove('overlayfade');
-  }, 160);
+overlay.addEventListener('click', function(e) {
+  console.log(e);
+  if (e.target === close || e.target === overlay) {
+    popup.classList.add('popupfade');
+    overlay.classList.add('overlayfade');
+    setTimeout(function() {
+      overlay.classList.add('none');
+      popup.classList.remove('popupfade');
+      overlay.classList.remove('overlayfade');
+    }, 160);
+  }
 });
 
 open.addEventListener('click', function() {
   overlay.classList.remove('none');
-
 });
